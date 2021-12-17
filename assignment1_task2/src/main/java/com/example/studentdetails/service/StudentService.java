745c01ae138d24d2da2ea48c3ff590f4a2add078
Katejs96 <84081279+Katejs96@users.noreply.github.com>
2021-12-17 11:02:07 -0400

    Add files via upload

---
+++ assignment1_task2/src/main/java/com/example/studentdetails/service/StudentService.java
@@ -0,0 +1,30 @@
+package com.example.studentdetails.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.studentdetails.domain.Student;
+import com.example.studentdetails.repository.StudentRepository;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repo;
+    public List<Student> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Student std) {
+        repo.save(std);
+    }
+
+    public Student get(long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+
+}
